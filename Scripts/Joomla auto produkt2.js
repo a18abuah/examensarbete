803ba00b11088daa67a110d99d440c7c33b1df95
a18abuah <asayid.9@hotmail.com>
2021-04-20 01:33:08 +0200

    Joomla dynamisk produkt

---
+++ Scripts/Joomla auto produkt2.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         New Userscript123 joomla book auto
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://localhost/joomla3.9/administrator/index.php?option=com_content&view=article&layout=edit&id=*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+
+
+    var imgage1 = "http://localhost/joomla3.9/images/Fitz-Porsche-Ultimate-Series_700x700.jpg";
+    var image1 = "images/Fitz-Porsche-Ultimate-Series_700x700.jpg"
+    document.getElementById('jform_image_thumb_image').value = image1;
+    document.getElementById('jform_image_main_image').value = image1;
+    document.getElementById('input-thumb-image').src = imgage1;
+    document.getElementById('input-main-image').src = imgage1;
+    var test = document.getElementById('input-main-image').src;
+    Joomla.submitbutton('article.save2new');
+})();
