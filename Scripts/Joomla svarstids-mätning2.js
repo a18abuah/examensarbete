712b19a316a7e07b79bd708437261347ad58f6ad
a18abuah <asayid.9@hotmail.com>
2021-04-21 14:16:42 +0200

    Joomla svarstids-mätning2

---
+++ Scripts/Joomla svarstids-mätning2.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Back to blog
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match       http://localhost/joomla3.9/index.php/shopping/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+    window.location.href = "http://localhost/joomla3.9/";
+})();
